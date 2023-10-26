cfefdf061b5aa804355bfc1b2fcbb8d52bc28490
mryang000000 <mryang000000@gmail.com>
2023-10-26 22:06:19 +0800

    copy clone files

--- test.ts
+++ test.ts
@@ -1,1 +1,32 @@
-// 在此处测试；当此软件包作为插件使用时，将不会编译此软件包。
+let item = 0
+OLED12864_I2C.init(60)
+OLED12864_I2C.rect(
+0,
+0,
+60,
+30,
+1
+)
+OLED12864_I2C.showString(
+0,
+0,
+"Hello!",
+1
+)
+OLED12864_I2C.showString(
+0,
+1,
+"1234567890",
+0
+)
+item = 0
+basic.forever(() => {
+    OLED12864_I2C.showNumber(
+    0,
+    3,
+    item,
+    1
+    )
+    item += 1
+    basic.pause(1000)
+})
