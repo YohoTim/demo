f757515fe93df3e91c81ab8b2035ba8c7d698e47
mryang000000 <mryang000000@gmail.com>
2023-11-14 21:08:07 +0800

    config with serial

--- BLINKIT.ts
+++ BLINKIT.ts
@@ -21,34 +21,11 @@ namespace BLINKIT {
      * @param s is the text will be show, eg: 'Hello!'
      * @param color is string color, eg: 1
      */
-    //% blockId="BLINKIT_I2C_SHOWSTRING" block="显示字符 x %x|y %y|字符 %s|颜色 %color"
+    //% blockId="BLINKIT_I2C_SHOWFIGURE" block="显示图案 字符 %s"
     //% weight=80 blockGap=8
     //% parts=BLINKIT_I2C trackArgs=0
-    export function showString(x: number, y: number, s: string, color: number = 1) {
-        let col = 0
-        let p = 0
-        let ind = 0
-        for (let n = 0; n < s.length; n++) {
-            p = font[s.charCodeAt(n)]
-            for (let i = 0; i < 5; i++) {
-                col = 0
-                for (let j = 0; j < 5; j++) {
-                    if (p & (1 << (5 * i + j)))
-                        col |= (1 << (j + 1))
-                }
-                ind = (x + n) * 5 * (_ZOOM + 1) + y * 128 + i * (_ZOOM + 1) + 1
-                if (color == 0)
-                    col = 255 - col
-                _screen[ind] = col
-                if (_ZOOM)
-                    _screen[ind + 1] = col
-            }
-        }
-        set_pos(x * 5, y)
-        let ind0 = x * 5 * (_ZOOM + 1) + y * 128
-        let buf = _screen.slice(ind0, ind + 1)
-        buf[0] = 0x40
-        pins.i2cWriteBuffer(_I2CAddr, buf)
+    export function showFigure(s: string) {
+        serial.writeString(s)
     }
 
     /**
@@ -73,27 +50,10 @@ namespace BLINKIT {
     //% weight=100 blockGap=8
     //% parts=BLINKIT_I2C trackArgs=0
     export function init(addr: number) {
-        _I2CAddr = addr;
-        cmd1(0xAE)       // SSD1306_DISPLAYOFF
-        cmd1(0xA4)       // SSD1306_DISPLAYALLON_RESUME
-        cmd2(0xD5, 0xF0) // SSD1306_SETDISPLAYCLOCKDIV
-        cmd2(0xA8, 0x3F) // SSD1306_SETMULTIPLEX
-        cmd2(0xD3, 0x00) // SSD1306_SETDISPLAYOFFSET
-        cmd1(0 | 0x0)    // line #SSD1306_SETSTARTLINE
-        cmd2(0x8D, 0x14) // SSD1306_CHARGEPUMP
-        cmd2(0x20, 0x00) // SSD1306_MEMORYMODE
-        cmd3(0x21, 0, 127) // SSD1306_COLUMNADDR
-        cmd3(0x22, 0, 63)  // SSD1306_PAGEADDR
-        cmd1(0xa0 | 0x1) // SSD1306_SEGREMAP
-        cmd1(0xc8)       // SSD1306_COMSCANDEC
-        cmd2(0xDA, 0x12) // SSD1306_SETCOMPINS
-        cmd2(0x81, 0xCF) // SSD1306_SETCONTRAST
-        cmd2(0xd9, 0xF1) // SSD1306_SETPRECHARGE
-        cmd2(0xDB, 0x40) // SSD1306_SETVCOMDETECT
-        cmd1(0xA6)       // SSD1306_NORMALDISPLAY
-        cmd2(0xD6, 1)    // zoom on
-        cmd1(0xAF)       // SSD1306_DISPLAYON
-        clear()
-        _ZOOM = 1
+        serial.redirect(
+            SerialPin.P8,
+            SerialPin.P12,
+            BaudRate.BaudRate9600
+            )
     }
 }
